93a45f4df3af6ab2033b10548e74046968df3599
pqy <pqy@seas.upenn.edu>
2020-04-07 19:44:55 -0400

    add tweet model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tweet(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from core.models import Tweet
 # Create your views here.
 def splash(request):
-	return render(request, "splash.html", {})
+	tweets = Tweet.objects.all()
+	return render(request, "splash.html", {"tweets" : tweets})

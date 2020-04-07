e79a00297655817ac50d06160718670243c0a079
pqy <pqy@seas.upenn.edu>
2020-04-07 19:27:56 -0400

    change background style

--- splash/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def splash(request):
-	return render(request, "splash.html", {})
--- twitterNext/urls.py
+++ twitterNext/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from splash.views import splash
+from core.views import splash
 
 urlpatterns = [
     path('admin/', admin.site.urls),

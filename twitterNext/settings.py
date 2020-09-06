82111b0c755cf8ef5e4c8ff2d38f1b65e136b2cb
unknown <pqy@seas.upenn.edu>
2020-09-05 21:04:13 -0400

    add heroku

--- twitterNext/settings.py
+++ twitterNext/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,5 @@ STATICFILES_DIRS = (
 )
 
 LOGIN_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
